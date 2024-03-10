ba7da2cf65f1708537ac0414d65e1e3eece359fa
Anna Torgunakova <annatorgun@mail.ru>
2024-03-10 22:38:39 +0700

    added user controller

---
+++ KeyBooking_backend/Dto/UsersListDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KeyBooking_backend.Dto
+{
+    public class UsersListDto
+    {
+        [Required]
+        public List<UserInfoDto> users { get; set; }
+
+        public UsersListDto(List<UserInfoDto> users)
+        {
+            this.users = users;
+        }
+    }
+}
---
+++ KeyBooking_backend/Services/UserService.cs
@@ -0,0 +1,51 @@
+﻿using KeyBooking_backend.Dto;
+using System.ComponentModel.DataAnnotations;
+
+namespace KeyBooking_backend.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public UserInfoDto GetUserInfo(string id)
+        {
+            var user = _context.Users.FirstOrDefault(x => x.Id == id);
+
+            if (user == null)
+            {
+                throw new ValidationException("This user does not exist");
+            }
+
+            UserInfoDto result = new UserInfoDto
+            {
+                UserId = user.Id,
+                UserName = user.UserName,
+                UserEmail = user.Email
+            };
+            return result;
+        }
+
+        public UsersListDto GetUsersInfo()
+        {
+            var users = _context.Users.ToList();
+            var result1 = new List<UserInfoDto>();
+            foreach (var user in users)
+            {   
+                var user1 = new UserInfoDto
+                {
+                    UserId = user.Id,
+                    UserName = user.UserName,
+                    UserEmail = user.Email
+                };
+                result1.Add(user1);
+            }
+            var result = new UsersListDto(result1);
+            return result;
+        }
+    }
+}
